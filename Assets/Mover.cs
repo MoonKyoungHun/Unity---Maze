daf4fc33032a8a6e6d57b1fced1d465c4c8454f2
MoonKyoungHun <lsf8355@gmail.com>
2024-08-21 10:40:29 +0900

    초기 커밋
    미로 찾기

---
+++ Assets/Mover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public int movePower;
+
+    public Rigidbody rigidbody;
+
+    //private void Start()
+    //{
+    //    rigidbody.AddForce(Vector3.up * movePower, ForceMode.Impulse);
+    //}
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(Vector3.left * movePower, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(Vector3.right * movePower, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Vector3.forward * movePower, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(Vector3.back * movePower, ForceMode.Force);
+        }
+    }
+
+}
